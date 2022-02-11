1d337a0531ba410f02d8f9c9abedb40a5e6aa98b
Ian O'Connell <ianoc@ianoc.net>
2022-02-10 17:23:26 -1000

    More cleanup and make it go

--- src/main/java/com/example/Cat.java
+++ src/main/java/com/example/Cat.java
@@ -3,9 +3,17 @@ package com.example;
 import com.animal.Animal;
 
 public class Cat implements Animal {
-    public String name = "Furry";asdfasfsdf
+    public String name = "Furry";
 
     public String feels_like() {
         return name;
     }
-}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}   
